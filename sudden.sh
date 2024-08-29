1736d82f6078152bb703e0afd06da2b0d209425d
WhiteCapella <whitecapella.official@gmail.com>
2024-08-29 12:46:48 +0900

    commit -1

--- sudden.sh
+++ sudden.sh
@@ -2,15 +2,13 @@
 #1/bin/bash
 
 FILE=~/tmp/_SUDDEN
-
+figlet KIA
 while true
 do
-	clear
 	if [ -e "$FILE" ]; then
 		DATE=$(date +"%Y%m%d %H:%M:%S")
 		echo $DATE "SUDDEN!!!"	
 	else
-		figlet KIA
 		DATE=$(date +"%Y%m%d %H:%M:%S")
 		echo $DATE "Check"
 	fi
